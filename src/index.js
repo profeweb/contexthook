da328c8a73fc7fcd6cfdffd5b2d381ecdfac8e11
Mitjanit <42413333+mitjanit@users.noreply.github.com>
2022-03-04 15:59:06 +0100

    context hook login

--- src/index.js
+++ src/index.js
@@ -1,17 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import { ContextProvider } from './Auth.context.js';
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+import App from './App.js'
+import "./styles.css";
+
+
+function AppWithProvider() {
+  return (
+    <ContextProvider value={500}>
+      <div className="card error fluid">
+        <h4>Login React Context</h4>
+      </div>
+      
+      <App />
+    </ContextProvider>
+  );
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<AppWithProvider />, rootElement);
